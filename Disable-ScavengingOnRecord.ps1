3235e5693075466f2bc42802a603c8d316516f8e
paratz <pablo@ratz.com.ar>
2017-03-17 17:17:30 -0300

    update
    update

---
+++ Disable-ScavengingOnRecord.ps1
@@ -0,0 +1,10 @@
+﻿
+#Cambiar por IP
+$record = Get-WmiObject -ComputerName dnsServerName -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_AType -filter "IPAddress='192.168.168.1'"
+$record.timeStamp = 0 # checkbox is unchecked,  3579756 is checked
+$record.psbase.put()
+
+#Cambiar por Nombre de Registro
+$record = Get-WmiObject -ComputerName dnsServerName -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_AType -filter "OwnerName='TMG.fabrikam.com.ar'"
+$record.timeStamp = 0 # checkbox is unchecked,  3579756 is checked
+$record.psbase.put()

1c84f78fbcf038b1af641fedc8d2024d55c80285
paratz <pablo@ratz.com.ar>
2017-03-20 09:04:15 -0300

    March20
    Adding support for input file with list of DNSRecords to modify

--- Disable-ScavengingOnRecord.ps1
+++ Disable-ScavengingOnRecord.ps1
@@ -1,10 +1,29 @@
-﻿
-#Cambiar por IP
-$record = Get-WmiObject -ComputerName dnsServerName -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_AType -filter "IPAddress='192.168.168.1'"
+﻿Param(
+  [string]$DomainName,
+  [string]$InputLogPath,
+  [string]$DNSServerName
+)
+
+#Example Run: .\Disable-ScavengingOnRecord.ps1 -DNSServerName dc01.contoso.com -DomainName contoso.com -InputLogPath C:\DNSData\Log.txt
+
+Start-Transcript
+
+Get-Content -Path $InputLogPath | ForEach-Object {  
+
+$DNSRecord = "$_.$DomainName"
+
+#Cambiar por Nombre de Registro
+$record = Get-WmiObject -ComputerName $DNSServerName -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_AType -filter "OwnerName='$DNSRecor'"
 $record.timeStamp = 0 # checkbox is unchecked,  3579756 is checked
 $record.psbase.put()
 
-#Cambiar por Nombre de Registro
-$record = Get-WmiObject -ComputerName dnsServerName -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_AType -filter "OwnerName='TMG.fabrikam.com.ar'"
-$record.timeStamp = 0 # checkbox is unchecked,  3579756 is checked
-$record.psbase.put()
+}
+
+Stop-Transcript
+
+#Cambiar por IP
+#$record = Get-WmiObject -ComputerName dnsServerName -Namespace 'root\MicrosoftDNS' -Class MicrosoftDNS_AType -filter "IPAddress='192.168.168.1'"
+#$record.timeStamp = 0 # checkbox is unchecked,  3579756 is checked
+#$record.psbase.put()
+
+

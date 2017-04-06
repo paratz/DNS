ec251e1e25e76eb7e013b96161ea67047a4f72de
paratz <pablo@ratz.com.ar>
2017-04-06 10:02:47 -0300

    upload single label domain

---
+++ Find-ReutilizedIPs.ps1
@@ -0,0 +1,25 @@
+﻿#Este script compara los registros del primer archivo .csv contra el segundo,
+#identidica si dos registros comparten la misma ip, pero no el mismo nombre
+#es util para encontrar aquellas direcciones IP que fueron reutilizadas con otro nombre
+
+
+$CandidatosABorrar = Import-Csv -Path "C:\DNSData\CandidatosQueRespondenIP.csv"
+
+$ZonaDNS = Import-Csv -Path "C:\DNSData\ExportZona14Marzo.csv"
+
+foreach($itemaborrar in $CandidatosABorrar)
+    {
+    
+        foreach($registro in $ZonaDNS)
+        {
+
+        If (($itemaborrar.IP -eq $registro.IP) -and ($itemaborrar.Host -ne $registro.Host))
+
+            {
+
+             $itemaborrar.Host + "comparte la misma IP que" + $registro.Host
+
+            } 
+
+        }
+    }
---
+++ Resolves-SingleLabelDomain.ps1
@@ -0,0 +1,54 @@
+﻿# This script will input a list of names, and then it will perform a dns lookup for different names:
+# zone name = the name on the list + dnssuffix
+# www name = the zone name with www. at the beginning of the string
+# intranet name = the name on the file, with a domain name appended
+#
+# example:
+#
+# .\Resolves-SingleLabelDomain.ps1 -InputLogPath C:\temp\zones.txt -OutputLogPath C:\temp\Results.csv -DominName contoso.com -DnsSuffix bg
+#
+# if the file zones.txt contains 2 lines: "domain1" and "domain2", it will resolve the following names:
+#
+# domain1.bg
+# www.domain1.bg
+# domain1.contoso.com
+# domain2.bg
+# www.domain2.bg
+# domain2.contoso.com
+#
+# and it will sabe the results to file "Results.csv"
+
+Param(
+  
+  #Inputlog path is a file that lists the single label name for the zone
+  [string]$InputLogPath,
+
+  [string]$OutputLogPath,
+
+  #Domain name is the domain that will be appended to resolve the shortname
+  [string]$DomainName,
+
+  #this is the top level domain name that needs to be added to the single label domain
+  [string]$dnssuffix
+
+)
+
+Start-Transcript
+
+Get-Content -Path $InputLogPath | ForEach-Object {  
+
+$FQDNName = "$_.$dnssuffix"
+
+Resolve-DnsName -Name $FQDNName | Export-Csv -Path $OutputLogPath -Append -NoTypeInformation -Force
+
+$wwwURL = "www.$_.$dnssuffix"
+
+Resolve-DnsName -Name $wwwURL | Export-Csv -Path $OutputLogPath -Append -NoTypeInformation -Force
+
+$IntranetName = "$_.$DomainName"
+
+Resolve-DnsName -Name $IntranetName | Export-Csv -Path $OutputLogPath -Append -NoTypeInformation -Force
+
+}
+
+Stop-Transcript
